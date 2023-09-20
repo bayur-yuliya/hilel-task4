65a6ede0619b6da033b56de9c7372b0e6736b140
Yuliya Bayur <juliy14497@gmail.com>
2023-09-20 22:21:41 +0300

    Add finally main.py

--- main.py
+++ main.py
@@ -12,22 +12,30 @@ def db_creator():
     connect = sqlite3.connect("task4.db")
     cursor = connect.cursor()
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS customers
-            (ID INTEGER PRIMARY KEY AUTOINCREMENT, 
-            first_name VARCHAR (20) NOT NULL, 
-            last_name VARCHAR (20) NOT NULL, 
-            email TEXT)
-            """)
+    try:
+        cursor.execute("""CREATE TABLE IF NOT EXISTS customers
+                (ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+                first_name VARCHAR (20) NOT NULL, 
+                last_name VARCHAR (20) NOT NULL, 
+                email TEXT)
+                """)
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS tracks
-            (ID INTEGER PRIMARY KEY AUTOINCREMENT, 
-            artist VARCHAR (50), 
-            length_in_seconds INT, 
-            release_date DATE)
-            """)
+        cursor.execute("""CREATE TABLE IF NOT EXISTS tracks
+                (ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+                artist VARCHAR (50), 
+                length_in_seconds INT, 
+                release_date DATE)
+                """)
 
-    connect.commit()
-    connect.close()
+        connect.commit()
+        connect.close()
+
+    except sqlite3.Error as error:
+        print("Помилка при підключенні до sqlite", error)
+
+    finally:
+        if connect:
+            connect.close()
 
 
 def data_creator():
@@ -54,6 +62,7 @@ def data_creator():
 def names():
     new_connect = sqlite3.connect("task4.db")
     new_cur = new_connect.cursor()
+
     res_query_first_name = new_cur.execute("SELECT COUNT(DISTINCT first_name) FROM customers ")
     unique_names = res_query_first_name.fetchall()
 
@@ -64,6 +73,7 @@ def names():
 def tracks():
     new_connect = sqlite3.connect("task4.db")
     new_cursor = new_connect.cursor()
+
     res_query_count_tracks = new_cursor.execute("SELECT COUNT(*) FROM tracks ")
     count_tracks = res_query_count_tracks.fetchall()
 
@@ -74,6 +84,7 @@ def tracks():
 def tracks_sec():
     new_connect = sqlite3.connect('task4.db')
     new_cursor = new_connect.cursor()
+
     res_query_tracks = new_cursor.execute('SELECT ID, length_in_seconds FROM tracks')
     all_tracks_info = ''
     tracks_info = res_query_tracks.fetchall()
